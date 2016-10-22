738a310d294f9c4041e66f75ddd49267bdfacba8
Mahan Hashemizadeh <mahanhz@gmail.com>
2016-10-22 21:39:13 +0200

    Make into a eureka client

--- src/main/java/com/amhzing/microservicesdashboard/MicroservicesDashboardApplication.java
+++ src/main/java/com/amhzing/microservicesdashboard/MicroservicesDashboardApplication.java
@@ -3,9 +3,11 @@ package com.amhzing.microservicesdashboard;
 import be.ordina.msdashboard.EnableMicroservicesDashboardServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableMicroservicesDashboardServer
+@EnableEurekaClient
 public class MicroservicesDashboardApplication {
 
 	public static void main(String[] args) {
